b9a7aee8678d79b785375460d3e3ddbdb075bee5
reetah589 <reetah589@gmail.com>
2021-11-03 12:22:08 -0500

    javascript data type notes

---
+++ index.js
@@ -0,0 +1,10 @@
+// in line comment 
+/* multi lined comment */
+
+/* Data Types:
+undefined: it hasnt been defined, variable that havent set to be anything yet  
+boolean: true or false
+null: means nothing, 
+string: any sort of text 
+symbol: immutable primitive value that is unique
+number: object */
