fc6afc2d9c743458f5de8cd9f9dd7757c0508608
reetah589 <reetah589@gmail.com>
2021-11-03 12:36:28 -0500

    moved js comments to html notes page

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+var myName = "Reetah"
+
+let ourName
+
+
 // in line comment 
 /* multi lined comment */
 
 /* Data Types:
-undefined: it hasnt been defined, variable that havent set to be anything yet  
+undefined: it hasnt been defined, variable that havent set to be anything yet 
 boolean: true or false
-null: means nothing, 
+null: means nothing
 string: any sort of text 
 symbol: immutable primitive value that is unique
-number: object */
+number: is a number
+object: can store a lot of different key value 
+*/
